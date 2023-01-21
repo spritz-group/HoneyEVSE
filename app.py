f955cf5a6ef87c78184ee04d706bc2e61397155d
Massimiliano Baldo <bmaxium01@gmail.com>
2023-01-21 17:26:56 +0100

    feature: create the path for "/" and "/status"

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+import pandas as pd
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
+generator = pd.read_csv("static/simulations.csv").iterrows()
+
+@app.route("/status")
+def status():
+    payload = str(next(generator)[1].to_json())
+    return Response(payload)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
