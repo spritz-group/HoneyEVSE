0c3e774ceda29b5450fa08672d10e9542b7a8c0c
Massimiliano Baldo <bmaxium01@gmail.com>
2023-02-02 18:11:24 +0100

    feature: support cycling iteration of csv file

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ generator = pd.read_csv("static/simulations.csv").iterrows()
 
 @app.route("/status")
 def status():
-    payload = str(next(generator)[1].to_json())
+    # refering to the global variable "generator"
+    global generator
+    value = next(generator, [None, None])[1]
+    # if the dataframe is terminated, the "value" becomes None
+    if (value is None):
+        # restarting the generator for the beginning
+        generator = pd.read_csv("static/simulations.csv").iterrows()
+        value = next(generator, [None, None])[1]
+    # creating the payload to be returned
+    payload = str(value.to_json())
     return Response(payload)
 
 @app.route("/")
