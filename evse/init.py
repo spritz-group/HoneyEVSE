ab9d5bd8dee97390811016e3b9956b6476512d44
Massimiliano Baldo <bmaxium01@gmail.com>
2023-02-08 12:39:14 +0100

    add: create the init.py file for initialize evse process

---
+++ evse/init.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Run this script just once to create the records of simulations for charging station.
+"""
+
+import signal
+import sys
+from pathlib import Path
+
+from utils.generate_power_charges import simulate
+
+NDEVICES = 10
+
+PATH = Path("./static/")
+
+def signal_handler(sig, frame):
+    print("Blocking the simulations...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == "__main__":
+    FILE_PATH = PATH / "simulations.csv"
+    if not FILE_PATH.exists():
+        print("Starting simulations.\nThis can take a while...")
+        print("PRESS CTRL + C FOR STOP THE SIMULATION!")
+        simulate(NDEVICES, FILE_PATH)
+        print("Generate simulations!")
+    else:
+        print("Already generate simulations!")
