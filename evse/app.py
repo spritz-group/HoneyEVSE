bd76b50caa7462d2b051218ffc160221f9b46a49
Massimiliano Baldo <bmaxium01@gmail.com>
2023-02-13 22:02:48 +0100

    refactor: adjust file path for json file

--- evse/app.py
+++ evse/app.py
@@ -7,7 +7,7 @@ def generator_json(file_path=""):
     for obj in json.load(open(file_path)):
         yield obj
 
-generator = generator_json("static/test.json")
+generator = generator_json("static/charges.json")
 
 # Route for request a new charges
 @app.route("/status")
@@ -18,7 +18,7 @@ def status():
     # if the list is terminated, the "value" becomes None
     if (value is None):
         # restarting the generator for the beginning
-        generator = generator_json("/test.json")
+        generator = generator_json("static/charges.json")
         value = next(generator, None)
     # return the payloaded value
     return value
