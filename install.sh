c5920969223f328ffc4b5b23dcf5e92b2a963ecc
Massimiliano Baldo <bmaxium01@gmail.com>
2023-02-18 17:09:14 +0100

    add: install script for the packages

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cwd=$(pwd)
+version=$(lsb_release -rs )
+
+# Wrong version warning
+if [ "$version" != "20.04" ] && [ "$version" != "18.04" ]
+then
+  printf "Warning! This installation script has only been tested on Ubuntu 20.04 LTS and 18.04 LTS and will likely not work on your Ubuntu version.\n\n"
+fi
+
+sleep 3
+
+# Update apt
+sudo apt update
+
+# Installing necessary packages
+sudo apt install -y git python3 python3-pip curl libsqlite3-dev openvswitch-testcontroller net-tools gnome-terminal
+
+# Install farpd
+sudo apt install -y farpd
+
+# CPPPO Correct Version 4.3.4
+sudo pip2 install cpppo==4.3.4
+
+# Installing Honeyd - just a copy and paste of the guide
+cd ~
+git clone https://github.com/DataSoft/Honeyd
+cd Honeyd
+sudo apt install -y libevent-dev libdumbnet-dev libpcap-dev libpcre3-dev libedit-dev bison flex libtool automake make zlib1g-dev
+./autogen.sh
+./configure
+make
+sudo make install
+
+# create log and make it modifiable
+touch honeyd.log
+sudo chmod 777 honeyd.log
+
+printf "\nInstallation finished.\n"
+exit 0;
